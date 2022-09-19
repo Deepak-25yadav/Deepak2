4bb76fa0bfac0493a98d810b5cfc73d9a3fdacaf
Deepak Yadav <25yadavdeepak@gmail.com>
2022-09-20 00:46:12 +0530

    Palindrome

---
+++ u1d18pr1palin.js
@@ -0,0 +1,13 @@
+let arr=["NAMAN"];
+let N=5;
+bag="";
+for(i=0; i<N; i++)
+  {
+    for(j=N-1-i; j>=N-1-i; j--)
+        {
+         if(str[i]==str[j]) 
+                                                      ;
+        }
+        }    
+  }
+console.log(bag);
